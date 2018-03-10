b86b6ad24ea21538bb71e4a70567fa3fbdcfeb03
EC2 Default User <ec2-user@ip-172-31-2-156.eu-west-1.compute.internal>
2018-03-10 00:32:38 +0000

    temporary change so db migration can be done

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'tdt4242@' + Rails.application.secrets.email_domain_name
+  #TODO: Fix this, this is a quick change so db:migrate can be run
+  config.mailer_sender = 'tdt4242@tdt4242.shop' #+ Rails.application.secrets.email_domain_name
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
