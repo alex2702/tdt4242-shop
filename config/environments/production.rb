64ae466a65b4b3225501bcccf202a138c46b78c5
alex2702 <wutfan@posteo.de>
2018-04-09 23:28:56 +0200

    production adjustment for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-
+  config.action_mailer.default_options = { from: Rails.application.secrets.email_sender }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
